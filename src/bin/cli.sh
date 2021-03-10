cc751baac2a61b7dba1544298f3153f3fb5d7f64
Kazuichi Takashiro <takashiro@qq.com>
2021-03-10 21:30:02 +0800

    Go back to the original folder (OS X)

--- src/bin/cli.sh
+++ src/bin/cli.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-cd "/Applications/wechatwebdevtools.app/Contents/MacOS/"
-
-#if [ $PWD == "/Application/wechatwebdevtoolsdeta.app/Contents/MacOS" ]
-#then
-#  '../Versions/86.0.4240.111/wechatwebdevtoolsdeta Helper.app/Contents/MacOS/node' ./cli.js "$@"
-#else
+cwd=$(pwd)
+cd '/Applications/wechatwebdevtools.app/Contents/MacOS/'
 '../Frameworks/nwjs Framework.framework/Helpers/wechatwebdevtools Helper (Renderer).app/Contents/MacOS/node' ./cli.js "$@"
-#fi
+cd "$cwd"
