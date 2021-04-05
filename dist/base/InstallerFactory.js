e7295e2d9c06f277667bd1357938668c4d187ae8
Kazuichi Takashiro <takashiro@qq.com>
2021-04-05 14:23:09 +0800

    Update to WeChat DevTools v1.05.2103190

--- dist/base/InstallerFactory.js
+++ dist/base/InstallerFactory.js
@@ -6,14 +6,14 @@ const Installer_1 = require("./Installer");
 function createDownloadLink(type, version) {
     return `https://servicewechat.com/wxa-dev-logic/download_redirect?type=${type}&from=mpwiki&download_version=${version}&version_type=1`;
 }
-const devtoolVersion = '1052102010';
+const devtoolVersion = '1052103190';
 exports.installSourceMap = {
     win32: new Installer_1.default({
         version: devtoolVersion,
         platform: 'x64',
         downloadUrl: createDownloadLink('x64', devtoolVersion),
         fileExtension: 'exe',
-        sha1sum: '40d4234eb919418005631a217be63af652e52d2b',
+        sha1sum: '94d4916b8edfd8a4ae79022b07ab74f7d633f116',
         installDir: 'C:\\Program Files (x86)\\Tencent\\微信web开发者工具', // Not configurable yet?
     }),
     darwin: new Installer_1.default({
@@ -21,7 +21,7 @@ exports.installSourceMap = {
         platform: 'darwin',
         downloadUrl: createDownloadLink('darwin', devtoolVersion),
         fileExtension: 'dmg',
-        sha1sum: '3b2e22ae91897721ab6179b0e2da2eb0d649c239',
+        sha1sum: 'a6efe498645431a78f592e0057dc3f22d9a96d15',
         installDir: '/Applications/wechatwebdevtools.app',
     }),
 };
--- src/base/InstallerFactory.ts
+++ src/base/InstallerFactory.ts
@@ -6,7 +6,7 @@ function createDownloadLink(type: string, version: string): string {
 	return `https://servicewechat.com/wxa-dev-logic/download_redirect?type=${type}&from=mpwiki&download_version=${version}&version_type=1`;
 }
 
-const devtoolVersion = '1052102010';
+const devtoolVersion = '1052103190';
 
 export const installSourceMap: Record<string, Installer> = {
 	win32: new Installer({
@@ -14,7 +14,7 @@ export const installSourceMap: Record<string, Installer> = {
 		platform: 'x64',
 		downloadUrl: createDownloadLink('x64', devtoolVersion),
 		fileExtension: 'exe',
-		sha1sum: '40d4234eb919418005631a217be63af652e52d2b',
+		sha1sum: '94d4916b8edfd8a4ae79022b07ab74f7d633f116',
 		installDir: 'C:\\Program Files (x86)\\Tencent\\微信web开发者工具', // Not configurable yet?
 	}),
 	darwin: new Installer({
@@ -22,7 +22,7 @@ export const installSourceMap: Record<string, Installer> = {
 		platform: 'darwin',
 		downloadUrl: createDownloadLink('darwin', devtoolVersion),
 		fileExtension: 'dmg',
-		sha1sum: '3b2e22ae91897721ab6179b0e2da2eb0d649c239',
+		sha1sum: 'a6efe498645431a78f592e0057dc3f22d9a96d15',
 		installDir: '/Applications/wechatwebdevtools.app',
 	}),
 };
